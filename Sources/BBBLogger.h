e521c43cd97ad90dd9c1581e8b90886ffa75d8ee
OTAKE Takayoshi <takayoshiotake@noreply.example>
2016-11-20 00:23:09 +0900

    Initial commit

---
+++ Sources/BBBLogger.h
@@ -0,0 +1,19 @@
+//
+//  BBBLogger.h
+//  BBBLogger
+//
+//  Created by OTAKE Takayoshi on 2016/11/20.
+//  Copyright © 2016 OTAKE Takayoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBBLogger.
+FOUNDATION_EXPORT double BBBLoggerVersionNumber;
+
+//! Project version string for BBBLogger.
+FOUNDATION_EXPORT const unsigned char BBBLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBBLogger/PublicHeader.h>
+
+
